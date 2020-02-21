ebcb77913d0f351101c1bae45619d5ce0b9c28e0
Sergey Petrov <sap4@ngs.ru>
2020-02-21 19:36:27 +0700

    Add les_04, remove les_05

--- lesson_5/task_2.py
+++
@@ -1,9 +0,0 @@
-"""Создать текстовый файл (не программно), сохранить в нем несколько строк, выполнить подсчет количества строк,
-количества слов в каждой строке."""
-
-with open('text_2.txt') as f_txt:
-    words_in_line = [len(line.strip().split()) for line in f_txt]
-
-print('Строк в файле:', len(words_in_line))
-for line, words in enumerate(words_in_line, 1):
-    print(f'Слов в строке {line}: {words}')
